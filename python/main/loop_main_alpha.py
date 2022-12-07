e7f890cdff64f917ee3320884cec0fd8d799fc3b
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-12-07 12:08:41 +0100

    Run-gaps settings, but with symmetries (and more samples per position).

--- python/main/loop_main_alpha.py
+++ python/main/loop_main_alpha.py
@@ -6,7 +6,7 @@ from torch.optim import AdamW
 from lib.data.file import DataFile
 from lib.games import Game
 from lib.loop import FixedSelfplaySettings, LoopSettings
-from lib.model.simple import DenseNetwork
+from lib.model.post_act import ScalarHead, PredictionHeads, ResTower, AtaxxConvPolicyHead
 from lib.selfplay_client import SelfplaySettings, UctWeights
 from lib.train import TrainSettings, ScalarTarget
 
@@ -17,15 +17,15 @@ def main():
     fixed_settings = FixedSelfplaySettings(
         game=game,
         muzero=False,
-        start_pos="default",
+        start_pos="random-gaps-v1",
 
         simulations_per_gen=200,
 
         cpu_threads_per_device=4,
-        gpu_threads_per_device=1,
+        gpu_threads_per_device=2,
         gpu_batch_size=1024,
         gpu_batch_size_root=0,
-        search_batch_size=16,
+        search_batch_size=8,
 
         saved_state_channels=0,
         eval_random_symmetries=True,
@@ -35,7 +35,7 @@ def main():
         temperature=1.0,
         zero_temp_move_count=30,
         q_mode="wdl+0.0",
-        max_game_length=400,
+        max_game_length=1000,
         dirichlet_alpha=0.2,
         dirichlet_eps=0.25,
         search_policy_temperature_root=1.4,
@@ -44,11 +44,11 @@ def main():
         search_fpu_child="relative+0",
         search_virtual_loss_weight=1.0,
         full_search_prob=1.0,
-        full_iterations=200,
+        full_iterations=1000,
         part_iterations=20,
         weights=UctWeights.default(),
-        cache_size=200,
-        top_moves=100,
+        cache_size=1000,
+        top_moves=0,
     )
 
     train_settings = TrainSettings(
@@ -66,29 +66,28 @@ def main():
     )
 
     def build_network(depth: int, channels: int):
-        return DenseNetwork(game, depth, channels, res=True)
-        # return PredictionHeads(
-        #     common=ResTower(depth, game.full_input_channels, channels),
-        #     scalar_head=ScalarHead(game.board_size, channels, 8, 128),
-        #     policy_head=AttentionPolicyHead(game, channels, channels),
-        # )
+        return PredictionHeads(
+            common=ResTower(depth, game.full_input_channels, channels),
+            scalar_head=ScalarHead(game.board_size, channels, 8, 128),
+            policy_head=AtaxxConvPolicyHead(game, channels),
+        )
 
-    # def dummy_network():
-    #     return build_network(1, 8)
+    def dummy_network():
+        return build_network(1, 8)
 
     def initial_network():
-        return build_network(16, 1024)
+        return build_network(16, 128)
 
     initial_files_pattern = ""
 
     # TODO implement retain setting, maybe with a separate training folder even
     settings = LoopSettings(
         gui=sys.platform == "win32",
-        root_path=f"data/loop/{game.name}/startup/",
+        root_path=f"data/loop/{game.name}/16x128_gaps/",
         port=63105,
         wait_for_new_network=True,
 
-        dummy_network=None,
+        dummy_network=dummy_network,
         initial_network=initial_network,
         initial_data_files=[DataFile.open(game, path) for path in glob.glob(initial_files_pattern)],
 
@@ -97,8 +96,8 @@ def main():
         min_buffer_size=1_500_000,
         max_buffer_size=2_000_000,
 
-        train_batch_size=128,
-        samples_per_position=0.3,
+        train_batch_size=512,
+        samples_per_position=2,
         test_fraction=0.05,
 
         optimizer=lambda params: AdamW(params, weight_decay=1e-3),
